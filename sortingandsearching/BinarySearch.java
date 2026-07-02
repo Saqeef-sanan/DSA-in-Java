e07de89492cdc1a2b7b7b9dc37d90409ca7ea1ad
Saqeef-sanan <saqeefsanan11@gmail.com>
2026-07-02 14:40:18 +0530

    array

---
+++ sortingandsearching/BinarySearch.java
@@ -0,0 +1,24 @@
+package sortingandsearching;
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9, 11, 13, 15};
+        int target = 7;
+        
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (arr[mid] == target) {
+                System.out.println("Target found at index: " + mid);
+                return;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println("Target not found in the array.");
+    }
+}
